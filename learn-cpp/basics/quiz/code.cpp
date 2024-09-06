cf9d49d97ab637b157e123271f98fdb4ae7d0fcc
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-09-06 16:44:00 +0530

    Added quiz

---
+++ learn-cpp/basics/quiz/code.cpp
@@ -0,0 +1,18 @@
+// simple quiz
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter another integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << x << " + " << y << " is " << x + y << ".\n";
+    std::cout << x << " - " << y << " is " << x - y << ".\n";
+
+    return 0;
+}
